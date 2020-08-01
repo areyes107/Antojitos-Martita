f861e1723bb52e47181a2cc2af33edbf99370542
Angel Reyes <angelmrg2000@hotmail.com>
2020-07-31 18:21:16 -0600

    components/footer/footer.component: created this component

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center mt-4">
+          &copy;{new Date().getFullYear()} | Made with 💖 by TCode <br />
+         <b> Telefono:</b> 54627157 <br/>
+         <b>Facebook Page:</b> "***" 
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
