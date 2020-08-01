580a26e734edd037f3d534d8cd944f5664751565
Angel Reyes <angelmrg2000@hotmail.com>
2020-07-31 18:15:46 -0600

    New components added

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Nav, Navbar } from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar bg="dark" position="fixed" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          alt="Hamburguer logo"
+          src="https://i.ibb.co/0m2Y0jG/Favicon.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+        Antojitos Martita
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Platillos</Nav.Link>
+          <Nav.Link href="#food">Combos</Nav.Link>
+          <Nav.Link href="#about">Acerca De</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
