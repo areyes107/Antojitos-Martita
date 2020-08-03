3e9abce3e257d9c5c593225cd97ab9eeb302f266
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-03 11:44:28 -0600

    components/footer/footer.component: fixed the jsx file

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './footer.component.css';
 
 const Footer = () => {
   return (
@@ -7,7 +8,7 @@ const Footer = () => {
         <p className="text-center mt-4">
           &copy;{new Date().getFullYear()} | Made with 💖 by TCode <br />
          <b> Telefono:</b> 54627157 <br/>
-         <b>Facebook Page:</b> "***" 
+         <b>Facebook Page:</b> <a href="https://www.facebook.com/comedor.martita.90"> Comedor Martita </a>
         </p>
       </footer>
     </>
